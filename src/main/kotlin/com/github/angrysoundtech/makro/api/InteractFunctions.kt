634d992a0d5b5a2ce069e9e9f3e9dde7f2bba665
AngrySoundTech <angrysoundtech@gmail.com>
2020-01-27 17:17:40 -0500

    Add Use and Eat

--- src/main/kotlin/com/github/angrysoundtech/makro/api/InteractFunctions.kt
+++ src/main/kotlin/com/github/angrysoundtech/makro/api/InteractFunctions.kt
@@ -3,12 +3,20 @@ package com.github.angrysoundtech.makro.api
 import com.github.angrysoundtech.makro.util.KeyBindUtil
 import net.minecraft.client.Minecraft
 
-object AttackFunctions {
+object InteractFunctions {
 
     private val mc = Minecraft.getMinecraft()
 
     fun attack(time: Long) {
         KeyBindUtil.holdKeybind(mc.gameSettings.keyBindAttack, time)
         Thread.sleep(time)
+    }
+
+    fun use(time: Long = 100) {
+        KeyBindUtil.holdKeybind(mc.gameSettings.keyBindUseItem, time)
+    }
+
+    fun eat() {
+        use(3000)
     }
 }
