0b7cc675a28cc05352519f68d2d0b24795bad955
AngrySoundTech <angrysoundtech@gmail.com>
2020-02-02 15:36:37 -0500

    Add macro root folder

--- src/main/kotlin/com/github/angrysoundtech/makro/ModConfig.kt
+++ src/main/kotlin/com/github/angrysoundtech/makro/ModConfig.kt
@@ -1,10 +1,12 @@
 package com.github.angrysoundtech.makro
 
+import net.minecraft.client.Minecraft
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.common.config.Config
 import net.minecraftforge.common.config.ConfigManager
 import net.minecraftforge.fml.client.event.ConfigChangedEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import java.io.File
 
 @Config(modid = Makro.ID)
 @Config.LangKey("makro.config.title")
@@ -14,6 +16,11 @@ object ModConfig {
     @Config.Comment("Should the macros be compiled on startup? If not, lazily.")
     @Config.LangKey("makro.config.compileonstart")
     var compileOnStart = true
+
+    @JvmField
+    @Config.Comment("Macro root folder")
+    @Config.LangKey("makro.config.folder")
+    var folder = Minecraft.getMinecraft().mcDataDir.canonicalPath + File.separator + "macros" + File.separator
 
     fun update() {
         ConfigManager.sync(Makro.ID, Config.Type.INSTANCE)
